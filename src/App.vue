29622ad662829590d066179d44b3628b08769b80
JairFonsecaP <jairfonsea64@yahoo.es>
2020-12-05 07:25:35 -0300

    gitignore, Se conecta api y muestra algunas cosas

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="app">
+    
+    <clima></clima>
   </div>
 </template>
 
 <script>
-
+import clima from './components/clima.vue'
 export default {
   name: 'App',
+  components: {
+    clima
+  }
 }
 </script>
 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
